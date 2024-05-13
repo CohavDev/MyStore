64ae08c41464e1d27d75d6089789200984106d8a
CohavDev <shaulspam@gmail.com>
2024-05-13 22:21:31 +0300

    server works

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var Express = require("express");
 var Mongoclient = require("mongodb").MongoClient;
 var cors = require("cors");
+var bodyParser = require("body-parser");
+const { ObjectId } = require("mongodb");
+
 var app = Express();
-// app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
 var CONNECTION_STRING =
   "mongodb+srv://shaulc:admin1234@cluster0.ywdnz4v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -51,9 +55,9 @@ app.post("/api/store/AddItem", (request, response) => {
   const cursor = client.db(DATABASENAME).collection("items");
   async function run() {
     try {
-      await cursor.insertOne(request.body.newItem);
+      const result = await cursor.insertOne(request.body.newItem);
       //   await cursor.insertOne({ id: 111111, name: "grapes" });
-      response.send("success");
+      response.send(result);
     } catch (error) {
       console.log(error);
       response.send("error");
@@ -65,7 +69,9 @@ app.post("/api/store/deleteItem", (request, response) => {
   const cursor = client.db(DATABASENAME).collection("items");
   async function run() {
     try {
-      await cursor.deleteOne({ id: request.body.deleteId });
+      await cursor.deleteOne({
+        _id: ObjectId.createFromHexString(request.body._id),
+      });
       //   await cursor.deleteOne({ id: 111111 });
       response.send("success");
     } catch (error) {
@@ -76,11 +82,15 @@ app.post("/api/store/deleteItem", (request, response) => {
   run();
 });
 app.post("/api/store/editItem", (request, response) => {
+  console.log("editing item...");
+  console.log(request.body._id);
+
   const cursor = client.db(DATABASENAME).collection("items");
   async function run() {
+    console.log(request.body._id);
     try {
       await cursor.updateOne(
-        { id: request.body.updateId },
+        { _id: ObjectId.createFromHexString(request.body._id) },
         { $set: request.body.updatedItem }
         // );
         //   await cursor.updateOne(
@@ -88,6 +98,28 @@ app.post("/api/store/editItem", (request, response) => {
         //     { $set: { name: "watermalon" } }
       );
       response.send("success");
+    } catch (error) {
+      console.log(error);
+      response.send("error");
+    }
+  }
+  run();
+});
+app.post("/api/store/searchItem", (request, response) => {
+  console.log("searching item...");
+  const cursor = client
+    .db(DATABASENAME)
+    .collection("items")
+    .find({ name: request.body.name });
+  async function run() {
+    console.log(request.body.name);
+    try {
+      const arr = [];
+      for await (const item of cursor) {
+        console.log(item);
+        arr.push(item);
+      }
+      response.send(arr);
     } catch (error) {
       console.log(error);
       response.send("error");
